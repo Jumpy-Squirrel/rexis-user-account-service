4cb2e4965dc6d182645328750b89e07df35806be
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2018-03-04 09:38:41 +0100

    initial commit spring initializr

---
+++ src/main/java/info/rexis/uas/RexisUserAccountServiceApplication.java
@@ -0,0 +1,12 @@
+package info.rexis.uas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexisUserAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RexisUserAccountServiceApplication.class, args);
+	}
+}
