0fb92e530927f5217b7d11c0f17d83e6ae554417
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2018-03-04 10:27:06 +0100

    running version with minimal sanitization

--- src/main/java/info/rexis/uas/RexisUserAccountServiceApplication.java
+++ src/main/java/info/rexis/uas/RexisUserAccountServiceApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RexisUserAccountServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RexisUserAccountServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RexisUserAccountServiceApplication.class, args);
+    }
 }
